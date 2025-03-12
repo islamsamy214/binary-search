70041cf1d5c8c9d2635a69b5ed188e33c30d30da
islam samy <64733740+islamsamy214@users.noreply.github.com>
2025-03-12 16:52:35 +0400

    Update binary-search.php

--- binary-search.php
+++ binary-search.php
@@ -1,37 +1,4 @@
 <?php
-class Search{
-    private $operations = 1;
-    
-    function binarySearch($array, $niddle){
-        $this->operations++;
-
-        sort($array);
-        $low = 0;
-        $high = count($array)-1;
-        if ($low <= $high) {
-            $mid = ceil(($low + $high)/2);
-            $guess = $array[$mid];
-            if($guess == $niddle){
-                return 'found and it did a '. $this->operations .' operations';
-            }else if ($guess < $niddle){
-                $array = array_slice($array, $mid, $high);
-                return $this->binarySearch($array,$niddle);
-            }else if ($guess > $niddle){
-                $array = array_slice($array,$low,$mid);
-                return $this->binarySearch($array,$niddle);
-            }
-        }
-
-        return 'it doesn\'t exist and it did '. $this->operations .' operations';
-    }
-
-}
-
-$arr = [11,55,22,5,6,78,1];
-$search = new Search();
-echo $search->binarySearch($arr,22);
-
-
 // with recursion
 class search
 {
@@ -61,6 +28,8 @@ class search
         if ($search < $mid) {
             return $this->binary_search(array_slice($list, 0, $mid_index), $search);
         }
+
+        return "its not found";
     }
 }
 $list = [1, 2, 3, 4, 5, 6, 7];
